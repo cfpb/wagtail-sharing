a124c733cd852344c873a0c527fe625de4f06fdc
Andy Chosak <chosak@users.noreply.github.com>
2017-02-22 11:25:00 -0500

    add sharing link in admin

--- wagtailsharing/wagtail_hooks.py
+++ wagtailsharing/wagtail_hooks.py
@@ -1,7 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 
+from django.utils.translation import ugettext_lazy as _
+from wagtail.wagtailadmin import widgets as wagtailadmin_widgets
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
+from wagtail.wagtailcore import hooks
 
+from wagtailsharing.helpers import get_sharing_url
 from wagtailsharing.models import SharingSite
 
 
@@ -14,3 +18,20 @@ class SharingSiteModelAdmin(ModelAdmin):
 
 
 modeladmin_register(SharingSiteModelAdmin)
+
+
+@hooks.register('register_page_listing_more_buttons')
+def add_sharing_link(page, page_perms, is_parent=False):
+    sharing_url = get_sharing_url(page)
+
+    if sharing_url:
+        yield wagtailadmin_widgets.Button(
+            'Get sharing link',
+            sharing_url,
+            attrs={
+                'title': _("Share latest revision of '{title}'").format(
+                    title=page.get_admin_display_title()
+                ),
+            },
+            priority=90
+        )
