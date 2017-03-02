fc5274e81f9ee781c7d74a8b48d50d05c23d78f5
Andy Chosak <chosak@users.noreply.github.com>
2017-03-02 17:14:10 -0500

    setup.py updates for pypi

--- setup.py
+++ setup.py
@@ -14,13 +14,30 @@ testing_extras = [
 ]
 
 
+short_description = 'Easier sharing of Wagtail drafts'
+
+
+def read_long_description():
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except (IOError, ImportError):
+        pass
+
+
 setup(
     name='wagtail-sharing',
-    version='0.1',
+    url='https://github.com/cfpb/wagtail-sharing',
+    author='CFPB',
+    author_email='tech@cfpb.gov',
+    license='CCO',
+    version='0.3',
     include_package_data=True,
     packages=find_packages(),
     install_requires=install_requires,
     extras_require={
         'testing': testing_extras,
-    }
+    },
+    description=short_description,
+    long_description=read_long_description()
 )
