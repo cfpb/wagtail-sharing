1d995f59fa0e2f2adf4a724e15f5c59b9c85cee4
Andy Chosak <chosak@users.noreply.github.com>
2017-10-31 10:13:40 -0400

    bump version for 0.5.1 release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author='CFPB',
     author_email='tech@cfpb.gov',
     license='CCO',
-    version='0.5',
+    version='0.5.1',
     include_package_data=True,
     packages=find_packages(),
     install_requires=install_requires,
