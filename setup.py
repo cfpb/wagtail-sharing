cfbde84d4ec7492203e5e6681750529b9d91ae31
nick.moreton <nick.moreton@torchbox.com>
2022-06-15 14:55:42 +0100

    Update setup.py and tox
    Just looking to get wagtail 3 and django 3.2 working with updates

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 install_requires = [
-    "wagtail>=2.7,<3",
+    "wagtail>=2.15,<4.0",
+    "django>=3.0,<4.0",
 ]
 
-testing_extras = ["coverage>=3.7.0"]
+testing_extras = ["coverage>=3.7.0", "mock>=3.0.0"]
 
 setup(
     name="wagtail-sharing",
@@ -22,14 +23,21 @@ setup(
     long_description=open("README.rst", "r", encoding="utf-8").read(),
     classifiers=[
         "Framework :: Django",
-        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
         "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
         "Framework :: Wagtail",
-        "Framework :: Wagtail :: 2",
+        "Framework :: Wagtail :: 2.15",
+        "Framework :: Wagtail :: 2.16",
+        "Framework :: Wagtail :: 3.0",
         "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
         "License :: Public Domain",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
