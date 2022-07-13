7fab3a7a06c3e7809c4c00f07df880e7adc37a3d
nick.moreton <nick.moreton@torchbox.com>
2022-07-13 14:12:11 +0100

    Refine setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import find_packages, setup
 
+
 install_requires = [
     "wagtail>=2.15,<4.0",
-    "django>=3.0,<5.0",
+    "django>=3.2,<5.0",
 ]
 
 testing_extras = ["coverage>=3.7.0"]
@@ -23,21 +24,14 @@ setup(
     long_description=open("README.rst", "r", encoding="utf-8").read(),
     classifiers=[
         "Framework :: Django",
-        "Framework :: Django :: 3.0",
-        "Framework :: Django :: 3.1",
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
         "Framework :: Wagtail",
-        "Framework :: Wagtail :: 2.15",
-        "Framework :: Wagtail :: 2.16",
-        "Framework :: Wagtail :: 3.0",
+        "Framework :: Wagtail :: 2",
+        "Framework :: Wagtail :: 3",
         "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
         "License :: Public Domain",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
     ],
 )
