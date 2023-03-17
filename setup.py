f44ff3273b325ae41bb553751f4c4c17b864a8bc
Andy Chosak <andy.chosak@cfpb.gov>
2023-03-17 11:10:17 -0400

    Migrate project configuration to pyproject.toml
    Mostly copying configuration from the implementation at
    https://github.com/cfpb/wagtail-inventory/blob/main/pyproject.toml

--- setup.py
+++ setup.py
@@ -1,36 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
-install_requires = [
-    "wagtail>=3",
-]
-
-testing_extras = ["coverage>=3.7.0"]
-
-setup(
-    name="wagtail-sharing",
-    url="https://github.com/cfpb/wagtail-sharing",
-    author="CFPB",
-    author_email="tech@cfpb.gov",
-    license="CCO",
-    version="2.7",
-    include_package_data=True,
-    packages=find_packages(),
-    python_requires=">=3.6",
-    install_requires=install_requires,
-    extras_require={"testing": testing_extras},
-    description="Easier sharing of Wagtail drafts",
-    long_description=open("README.rst", "r", encoding="utf-8").read(),
-    classifiers=[
-        "Framework :: Django",
-        "Framework :: Django :: 3.2",
-        "Framework :: Django :: 4",
-        "Framework :: Wagtail",
-        "Framework :: Wagtail :: 3",
-        "Framework :: Wagtail :: 4",
-        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
-        "License :: Public Domain",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-    ],
-)
+setup()
